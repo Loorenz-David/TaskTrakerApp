31c089193f24eb7c159a9bb4f0c2c7a6491f8c90
David <loorenz.david@gmail.com>
2025-05-06 07:23:20 +0200

    made dismantler to activate section id 2

--- taskTrakerAppV1/Routers/custom_decorators.py
+++
@@ -1,2 +0,0 @@
-from flask import redirect,url_for
-from flask_login import current_user
--- taskTrakerAppV1/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from .sections_model import Sections, Items, Items_Sections, Tasks, Tasks_Items
-
-from .user_model import Users
